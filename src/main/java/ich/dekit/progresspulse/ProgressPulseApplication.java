2907e18f73fd909feabc8ed5816bdfefa3da8f52
Umar Hamzathanov <umar.hamzathanov@uco.kz>
2023-05-31 07:42:33 +0600

    Initial commit

---
+++ src/main/java/ich/dekit/progresspulse/ProgressPulseApplication.java
@@ -0,0 +1,13 @@
+package ich.dekit.progresspulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ich/dekit/progresspulse/ProgressPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package ich.dekit.progresspulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
